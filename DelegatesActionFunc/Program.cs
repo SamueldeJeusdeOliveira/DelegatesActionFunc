1e880ce0a80f7c89c3e2205b5b998ab9eefc8985
SamueldeJeusDeOliveira <samuksjj@gmail.com>
2025-03-29 22:24:46 -0300

    commit didádico

--- DelegatesActionFunc/Program.cs
+++ DelegatesActionFunc/Program.cs
@@ -11,7 +11,7 @@ namespace Course
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            list.RemoveAll(ProductTest);
+            list.RemoveAll(p => p.Price >= 100.0);
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
